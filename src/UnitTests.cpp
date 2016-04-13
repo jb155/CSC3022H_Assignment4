5993dc1c8ff79d1b02a42d92c288bdaea9f6d1bc
Jacques Botha <BTHJAC013@myuct.ac.za>
2016-04-13 16:35:11 +0200

    Did basic setup for main class

---
+++ src/UnitTests.cpp
@@ -0,0 +1,2 @@
+#include "Image.h"
+#include "catch.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,41 @@
 #include "Image.h"
 
 int main(int argc, const char* argv[]) {
+	if (argc < 2) {
+        std::cout << "Too few arguments." << std::endl;
+        return 0;
+    }
 
+    std::string command = argv[1];
+
+    string image1Name = argv[2];
+    BTHJAC013::Image image1;
+    image1 << image1Name;
+
+    BTHJAC013::Image image2;
+
+    std::string outputName = argv[argc-1];
+    BTHJAC013::Image outputImage;
+
+    if (command == "-a") { //Add
+        image2 << argv[3];
+        outputImage = image1 + image2;
+    } else if (command == "-s") { //Subtract
+        image2 << argv[3];
+        outputImage = image1 - image2;
+    } else if (command == "-i") { //Invert
+        outputImage = !image1;
+        outputImage >> outputName;
+    } else if (command == "-l") { //Mask
+        image2 << argv[3];
+        outputImage = image1 / image2;
+    } else if (command == "-t") { //Threshold
+        int threshold = stoi(argv[3]);
+        outputImage = image1 * threshold;
+    } else {
+        cout << "Command: " << command << " is not recognized as a legitimate command.";
+        return 0;
+    }
+    imgOut >> outputName;
+    return 0;
 }
